77074ce2433cbc56e041ef4ab814cbbbd1fbede7
kimjunha <tto9011@naver.com>
2022-10-05 18:37:56 +0900

    초기세팅 끝

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learnd ddddw
+          Learnd ddddwㅇ
         </a>
       </header>
     </div>
