ba98a7e84d0b9b9f40bf0884822521e6911a9781
kimjunha <tto9011@naver.com>
2022-10-23 04:16:16 +0900

    에브리타임nav만들기

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import {navIcons} from '../path/Resources'
+import { navIcons } from '../path/Resources'
 const Nav = styled.div`
     display:flex;
     justify-content:space-between;
+    align-items: center;
+    height:50px;
+    width:100%;
 `;
 const NavBox = styled.div`
-    
 `;
 const NavBoxIcon = styled.div`
     display:flex;
-    margin:10px;
+    justify-content:end;
 `;
 const NavEvery = styled.div`
     font-size:10px;
@@ -20,14 +22,11 @@ const NavSsu = styled.div`
     font-size:20px;
     font-weight: bold;
 `;
-const NavSearchIcon = styled.div` 
-    font-size:20px;
-    padding-right:30px;
+const NavSearchIcon = styled.img` 
+    width: 10%;
 `;
 const NavPersonIcon = styled.img`
   width: 10%;
-  margin-right: 2px;
-  backgorundcolor:red;
 `;
 
 
@@ -40,10 +39,14 @@ const MainPage = () => {
                 <NavSsu>숭실대</NavSsu>
             </NavBox>
             <NavBoxIcon>
-                <NavSearchIcon>돋보기 아이콘</NavSearchIcon>
+                
+                <NavSearchIcon
+                    src={navIcons.search}
+                    alt="검색"
+                />
                 <NavPersonIcon
-                src={navIcons.person}
-                alt="프로필"
+                    src={navIcons.person}
+                    alt="프로필"
                 />
             </NavBoxIcon>
         </Nav>
--- src/path/Resources.js
+++ src/path/Resources.js
@@ -1,6 +1,8 @@
 import person from "../img/person.png";
+import search from "../img/search.png";
 
 export const navIcons = {
    person,
+   search,
   };
   
