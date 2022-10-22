22c2e7f823c33854427c04bbdee516eb45b68545
kimjunha <tto9011@naver.com>
2022-10-22 23:05:45 +0900

    시작!

---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  padding: 6px 12px;
+  border-radius: 8px;
+  font-size: 1rem;
+  line-height: 1.5;
+  border: 1px solid lightgray;
+  color: red;
+  background: blue;
+`;
+function Main() {
+  return (
+    <StyledButton>
+      <div>기무성 바보</div>
+      
+    </StyledButton>
+
+  );
+}
+
+export default Main;
