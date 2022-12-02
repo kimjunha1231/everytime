78343a245a0d7ec97dcdd8026e7a3f834cd09b91
junha kim <tto9011@naver.com>
2022-12-02 23:44:25 +0900

    프로필

--- src/components/profile/Info.js
+++ src/components/profile/Info.js
@@ -3,12 +3,49 @@ import styled from "styled-components";
 import { navIcons } from '../../path/Resources'
 
 const InfoBox = styled.div`
-    margin-top: 50px;
+    margin-top: 70px;
+    border-radius: 20px;
+    border: 1.2px solid #d3d3d3;
+    padding: 15px;
+    display: flex;
+    align-items: center;
 `;
 
+const PersonIcon = styled.img`
+    width: 60px;
+`;
+const InfoContent = styled.div`
+`;
+
+const InfoId = styled.div`
+    font-size: 13px;
+    font-weight: 500;
+`;
+const InfoName = styled.div`
+    font-size: 11px;
+    color: #dbdbdb;
+    font-weight: bold;
+    margin-top: 3px;
+`;
+
+
 const Info = () => {
-    return(
-        <InfoBox>띠용</InfoBox>
+    return (
+        <InfoBox>
+            <PersonIcon src={navIcons.person} alt="프로필"></PersonIcon>
+            <InfoContent>
+                <InfoId>
+                    cht8949
+                </InfoId>
+                <InfoName>
+                    김준하 / 쑹씨리
+                </InfoName>
+                <InfoName>
+                    숭실대20학번
+                </InfoName>
+            </InfoContent>
+        </InfoBox>
+
     );
 }
 
