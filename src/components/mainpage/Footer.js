0e4212a006770b95ffaffa98154b0dad39253171
junha kim <tto9011@naver.com>
2022-12-08 19:26:32 +0900

    dd

---
+++ src/components/mainpage/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export default const Footer = () => {
+    let navigate = useNavigate();
+    const [title, setTitle] = useRecoilState(navtitle)
+    return (
+        <>
+        </>
+    );
+};
